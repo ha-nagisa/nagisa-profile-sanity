a50f4b63622544bd9a7562d4c203ce30152f8f55
ha-nagisa <nagisa18222@icloud.com>
2021-06-13 11:03:13 +0900

    schemaの修正

--- schemas/schema.js
+++ schemas/schema.js
@@ -11,6 +11,217 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      name:'author',
+      type:'document',
+      title:'Author',
+      fields:[
+        {
+          name: 'name',
+          title:'Name',
+          type:'string',
+        },
+        {
+          name: 'avatar',
+          title:'Avatar',
+          type:'image',
+        }
+      ]
+    },
+    {
+      name:'blog',
+      type:'document',
+      title:'Blog',
+      fields: [
+        {
+          name: 'title',
+          type:'string',
+          title:'Title',
+        },
+        {
+          name: 'summary',
+          type:'text',
+          title:'Summary'
+        },
+        {
+          name: 'coverImage',
+          title:'Cover Image',
+          type:'image',
+          options: {
+            hotspot: true
+          },
+          fields: [
+            {
+              type:'text',
+              name:'alt',
+              title:'Description',
+            }
+          ]
+        },
+        {
+          name: 'content',
+          title:'Content',
+          type:'array',
+          of: [
+            {
+              type:'block'
+            },
+            {
+              type:'image',
+              fields: [
+                {
+                  title: 'Image Position',
+                  name: 'position',
+                  type:'string',
+                  options: {
+                    list:[
+                      {title: 'Center', value: 'center'},
+                      {title: 'Left', value: 'left'},
+                      {title: 'Right', value: 'right'},
+                    ],
+                    layout:'radio',
+                    isHighlighted:true
+                  }
+                },
+                {
+                  type:'text',
+                  name:'alt',
+                  title:'Description',
+                  options: {
+                    isHighlighted:true
+                  }
+                }
+              ],
+              options: {
+                hotspot: true
+              }
+            },
+            {
+              type: 'code',
+              options: {
+                withFilename:true
+              }
+            }
+          ]
+        },
+        {
+          name: 'date',
+          title:'Date',
+          type:'datetime',
+          validation:(Rule) => Rule.required() 
+        },
+        {
+          name: 'author',
+          title:'Author',
+          type:'reference',
+          to:[{type:'author'}],
+          validation:(Rule) => Rule.required()
+        },
+        {
+          name: 'slug',
+          type:'slug',
+          title:'Slug',
+          validation:(Rule) => Rule.required().error('My custom message')
+        }
+      ]
+    },
+    {
+      name:'work',
+      type:'document',
+      title:'Work',
+      fields: [
+        {
+          name: 'title',
+          type:'string',
+          title:'Title',
+        },
+        {
+          name: 'summary',
+          type:'text',
+          title:'Summary'
+        },
+        {
+          name: 'coverImage',
+          title:'Cover Image',
+          type:'image',
+          options: {
+            hotspot: true
+          },
+          fields: [
+            {
+              type:'text',
+              name:'alt',
+              title:'Description',
+            }
+          ]
+        },
+        {
+          name: 'content',
+          title:'Content',
+          type:'array',
+          of: [
+            {
+              type:'block'
+            },
+            {
+              type:'image',
+              fields: [
+                {
+                  title: 'Image Position',
+                  name: 'position',
+                  type:'string',
+                  options: {
+                    list:[
+                      {title: 'Center', value: 'center'},
+                      {title: 'Left', value: 'left'},
+                      {title: 'Right', value: 'right'},
+                    ],
+                    layout:'radio',
+                    isHighlighted:true
+                  }
+                },
+                {
+                  type:'text',
+                  name:'alt',
+                  title:'Description',
+                  options: {
+                    isHighlighted:true
+                  }
+                }
+              ],
+              options: {
+                hotspot: true
+              }
+            },
+            {
+              type: 'code',
+              options: {
+                withFilename:true
+              }
+            }
+          ]
+        },
+        {
+          name: 'date',
+          title:'Date',
+          type:'datetime',
+          validation:(Rule) => Rule.required() 
+        },
+        {
+          name: 'author',
+          title:'Author',
+          type:'reference',
+          to:[{type:'author'}],
+          validation:(Rule) => Rule.required()
+        },
+        {
+          name: 'slug',
+          type:'slug',
+          title:'Slug',
+          validation:(Rule) => Rule.required().error('My custom message')
+        }
+      ]
+    }
   ]),
 })
+
